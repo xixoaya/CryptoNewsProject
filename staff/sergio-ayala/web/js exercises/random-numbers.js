a93722cfdcbd575b60c34541fec266da5f492c1f
Sergio Ayala <sergio.ayala51@gmail.com>
2021-10-09 20:06:15 +0200

    modify random numbers exercise with the solution

--- staff/sergio-ayala/web/js exercises/random-numbers.js
+++ staff/sergio-ayala/web/js exercises/random-numbers.js
@@ -1,14 +1,15 @@
 function randomNumbers(n, m) {
     
     var arr = [];
-    for (var i=0, t=40; i<t; i++) {
-    arr.push(Math.round(Math.random() * t))
+    for (var i=0, t=n; i<t; i++) {
+    arr.push(Math.round(Math.random(m) * t))
     }
     console.log(arr);
 
     return arr;
 }
 
+console.log(randomNumbers(20, 26))
 
 // Debemos generar un vector (array) de números aleatorios
 // El array debe tener una longitud de n
--- staff/sergio-ayala/web/js exercises/random-numbers.test.js
+++ staff/sergio-ayala/web/js exercises/random-numbers.test.js
@@ -1,1 +1,29 @@
 // Implementar los tests que consideres necesarios
+
+describe('TEST RANDOM NUMBERS')
+
+// CASE 1
+
+var res = randomNumbers(15, 45)
+
+
+if ( res instanceof Array 
+    && res.length === 15
+    && Math.max(...res) < 45
+    ) { success('TEST OK')
+} else { fail('TEST FAILED!!')
+    
+}
+
+// CASE 2
+
+var res = randomNumbers(2, 3)
+
+
+if ( res instanceof Array 
+    && res.length === 2
+    && Math.max(...res) < 3
+    ) { success('TEST OK')
+} else { fail('TEST FAILED!!')
+    
+}
